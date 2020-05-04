0cf2fb6b237d5584d47d8ffb1baca54ac5b2a30a
Anne-Sophie Leber <anne-sophie@macbook-air-de-anne-sophie.home>
2020-05-04 11:55:57 +0200

    origin

---
+++ ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-cham <ade-cham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/04 11:27:49 by ade-cham          #+#    #+#             */
+/*   Updated: 2020/05/04 11:55:05 by ade-cham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int ft_atoi(const char *str)
+{
+    int i;
+    int sign;
+    int result;
+
+    i = 0;
+    sign = 1;
+    result = 0;
+    while (str[i] != '\0' &&  (str[i] == ' ' || str[i] == '\t' || str[i] == '\f' ||
+			str[i] == '\r' || str[i] == '\n' || str[i] == '\v'))
+		i++;
+    while (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            sign *= -1;
+        i++;
+    }
+    while (str[i] => 'O' && str[i] =< '9')
+    {
+        result = result * 10 + (str[i] - 48);
+        i++;
+    }
+    return (sign * result)
+}
