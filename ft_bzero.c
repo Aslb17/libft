0cd9ecc9277c8b253bc9de208507fde1e515a015
Anne-Sophie Leber <anne-sophie@macbook-air-de-anne-sophie.home>
2020-05-05 17:16:15 +0200

    origin

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ade-cham <ade-cham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/05/05 16:18:03 by ade-cham          #+#    #+#             */
-/*   Updated: 2020/05/05 17:15:13 by ade-cham         ###   ########.fr       */
+/*   Created: 2020/05/05 17:13:00 by ade-cham          #+#    #+#             */
+/*   Updated: 2020/05/05 17:15:17 by ade-cham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void    *ft_memset(void *dest, int c, size_t n)
+void    *ft_bzero(void *dest, size_t n)
 {
     int i;
     unsigned char *ptr;
-  
+
     ptr = (unsigned char *) dest;
     i = 0;
     while (i < n)
     {
-        ptr[i] = (unsigned char) c;
+        ptr[i] = 0;
         i++;
     }
     return (dest);
