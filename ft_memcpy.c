b374402db1aa0ba724998dfdca9186df35508031
Anne-Sophie Leber <anne-sophie@macbook-air-de-anne-sophie.home>
2020-05-08 14:42:25 +0200

    origin

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,23 +6,25 @@
 /*   By: ade-cham <ade-cham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/06 11:04:35 by ade-cham          #+#    #+#             */
-/*   Updated: 2020/05/06 15:18:20 by ade-cham         ###   ########.fr       */
+/*   Updated: 2020/05/08 14:41:52 by ade-cham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
 void    *ft_memcpy(void *dest, const void *src, size_t n)
 {
-    int i;
     unsigned char *ptr_dest;
     unsigned char *ptr_src;
 
-    i = 0;
     ptr_dest = (unsigned char *) dest;
     ptr_src = (unsigned char *) src;
-    while (i > n)
+    while (n > 0)
     {
-        ptr_dest[i] = ptr_src[i];
-        i++;
+        *ptr_dest = *ptr_src;
+        ptr_dest++;
+        ptr_src++;
+        n--;
     }
     return (dest);
 }
