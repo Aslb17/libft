8a778cdeaac7850e092a2d995bd70addb3e9c94e
Anne-Sophie Leber <anne-sophie@macbook-air-de-anne-sophie.home>
2020-05-08 14:28:01 +0200

    origin

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,21 +6,21 @@
 /*   By: ade-cham <ade-cham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 17:13:00 by ade-cham          #+#    #+#             */
-/*   Updated: 2020/05/05 17:15:17 by ade-cham         ###   ########.fr       */
+/*   Updated: 2020/05/08 14:26:48 by ade-cham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
 void    *ft_bzero(void *dest, size_t n)
-{
-    int i;
+{ 
     unsigned char *ptr;
 
     ptr = (unsigned char *) dest;
-    i = 0;
-    while (i < n)
+    while (n > 0)
     {
-        ptr[i] = 0;
-        i++;
+       *ptr = 0;
+        n--;
     }
     return (dest);
 }
