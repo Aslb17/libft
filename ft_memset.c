6491cbce33e0d6a5bd08ea73ac172f230352cbe6
Anne-Sophie Leber <anne-sophie@macbook-air-de-anne-sophie.home>
2020-05-08 14:46:11 +0200

    origin

--- ft_memset.c
+++ ft_memset.c
@@ -6,21 +6,22 @@
 /*   By: ade-cham <ade-cham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/05 16:18:03 by ade-cham          #+#    #+#             */
-/*   Updated: 2020/05/05 17:15:13 by ade-cham         ###   ########.fr       */
+/*   Updated: 2020/05/08 14:45:06 by ade-cham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
 void    *ft_memset(void *dest, int c, size_t n)
 {
-    int i;
     unsigned char *ptr;
   
     ptr = (unsigned char *) dest;
-    i = 0;
-    while (i < n)
+    while (n > 0)
     {
-        ptr[i] = (unsigned char) c;
-        i++;
+        *ptr = (unsigned char) c;
+        ptr++;
+        n--;
     }
     return (dest);
 }
