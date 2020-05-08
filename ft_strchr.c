27cf372c02bf94dd6473ae0ae516270d8965666b
Anne-Sophie Leber <anne-sophie@macbook-air-de-anne-sophie.home>
2020-05-08 16:42:00 +0200

    origin

---
+++ ft_strchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-cham <ade-cham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/01 13:52:18 by ade-cham          #+#    #+#             */
+/*   Updated: 2020/05/08 16:41:21 by ade-cham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+char    *ft_strchr(const char *s, int c)
+{	
+    unsigned int	i;
+    char            *str;
+    char            tofind;
+
+	i = 0;
+    str = (char*)s;
+    tofind = (char) c;
+	while (str[i] != tofind)
+	{
+		if (str[i] == '\0')
+			return (NULL);
+		i++;
+	}
+	return ((char*)str +i);
+}
