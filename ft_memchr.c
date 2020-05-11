19a3e12177119a20e63b3d4059305393e9953338
Anne-Sophie Leber <anne-sophie@macbook-air-de-anne-sophie.home>
2020-05-11 16:07:53 +0200

    origin

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-cham <ade-cham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/11 16:05:13 by ade-cham          #+#    #+#             */
+/*   Updated: 2020/05/11 16:07:27 by ade-cham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*src;
+	size_t			i;
+
+	if (s == NULL || n == 0)
+		return (NULL);
+	src = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (src[i] == (unsigned char)c)
+			return ((void *)(src + i));
+		i++;
+	}
+	return (0);
+}
