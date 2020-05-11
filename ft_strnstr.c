d5c1fa421273d286c6640139e9d3b5930e79cda4
Anne-Sophie Leber <anne-sophie@macbook-air-de-anne-sophie.home>
2020-05-11 14:49:25 +0200

    origin

---
+++ ft_strnstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-cham <ade-cham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/11 14:35:15 by ade-cham          #+#    #+#             */
+/*   Updated: 2020/05/11 14:48:57 by ade-cham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	if (little[0] == '\0')
+		return ((char *)big);
+	while (big[i] != '\0' && i < len)
+	{
+		if (big[i] == little[0])
+		{
+			j = 0;
+			while (big[i + j] == little[j]
+					&& i + j < len)
+			{
+				j++;
+				if (little[j] == '\0')
+					return ((char *)&big[i]);
+			}
+		}
+		i++;
+	}
+	return (NULL);
+}
