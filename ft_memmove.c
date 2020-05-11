d6a34e2d1edd8453da08982174898ce0f78a0842
Anne-Sophie Leber <anne-sophie@macbook-air-de-anne-sophie.home>
2020-05-11 16:00:46 +0200

    origin

---
+++ ft_memmove.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-cham <ade-cham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/11 15:27:29 by ade-cham          #+#    #+#             */
+/*   Updated: 2020/05/11 16:00:05 by ade-cham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memmove(void *dest, const void *src, size_t len)
+{
+    size_t  i;  
+
+    i = 0;	
+	if (dest == NULL || src == NULL)
+		return (NULL);
+	if (dest < src)
+	{
+		while (i < len)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
+	}
+	else
+	{
+	    while (len > 0)
+        {
+            ((unsigned char *)dest)[len - 1] = ((unsigned char*)src)[len - 1];
+			len--;
+		}
+	}
+	return ((void *)dest);
+}
