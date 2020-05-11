e3e98013386a0d0b0f1408a25b5e4702e25951b9
Anne-Sophie Leber <anne-sophie@macbook-air-de-anne-sophie.home>
2020-05-11 11:42:02 +0200

    origin

---
+++ ft_strncmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-cham <ade-cham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/11 11:02:22 by ade-cham          #+#    #+#             */
+/*   Updated: 2020/05/11 11:41:17 by ade-cham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int		ft_strncmp(const char *str1, const char *str2, size_t n)
+{
+	size_t	i;
+
+	i  = 0;
+	if (n == 0 || str1 == NULL || str2 == NULL)
+		return (0);
+	while (str1[i] != '\0' && str2[i] != '\0'
+			&& str1[i] == str2[i]
+			&& i < n - 1)
+	        i++;
+	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+}
