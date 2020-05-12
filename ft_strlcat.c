ead110ea19f2507bf7e3e900d28fd6a458544293
Anne-Sophie Leber <anne-sophie@macbook-air-de-anne-sophie.home>
2020-05-12 17:19:17 +0200

    origin

---
+++ ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-cham <ade-cham@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/12 17:06:38 by ade-cham          #+#    #+#             */
+/*   Updated: 2020/05/12 17:18:43 by ade-cham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h> 
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	src_size;
+	size_t	dest_size;
+
+    src_size = ft_strlen(src);
+	dest_size = ft_strlen(dest);
+	i = 0;
+	if (size == 0)
+		return (src_size);
+	if (dest_size > size)
+		return (src_size + size);
+	while (src[i] != '\0' && (i + dest_size + 1) < size)
+	{
+		dest[i + dest_size] = src[i];
+		i++;
+	}
+	dest[i + dest_size] = '\0';
+	return (dest_size + src_size);
+}
